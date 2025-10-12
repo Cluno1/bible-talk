4b15c690ffda21a179567fec7754e05c02f822de
zld <17875477802@163.com>
2025-10-12 20:57:24 +0800

    config something

--- src/utils/default/album/index.ts
+++ src/utils/default/album/index.ts
@@ -1,5 +1,7 @@
 import type { MusicAlbum } from "@/type/music";
 import { musicdemo_zld } from "./zld";
 import { ihopeAlbum } from "./ihope";
+//本地查询
+export const localMusicAlbums: MusicAlbum[] = [ihopeAlbum];
 
-export const localMusicAlbums:MusicAlbum[]=[musicdemo_zld,ihopeAlbum]
+export const secretAlbums: MusicAlbum[] = [musicdemo_zld];
